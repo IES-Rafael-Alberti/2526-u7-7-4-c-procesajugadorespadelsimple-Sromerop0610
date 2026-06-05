d6adc3bd38088bb13e4930b1844f9b119a4bf434
Sara Romero <sara.romerop256@gmail.com>
2026-06-05 16:21:30 +0200

    añadiendo el metodo que escribe el fichero de los partidos

--- src/main/kotlin/org/iesra/procesapadel/domain/infrastructure/OutputWriter.kt
+++ src/main/kotlin/org/iesra/procesapadel/domain/infrastructure/OutputWriter.kt
@@ -1,6 +1,7 @@
 package org.iesra.procesapadel.domain.infrastructure
 
 import org.iesra.procesapadel.domain.model.Couple
+import org.iesra.procesapadel.domain.model.Match
 import org.iesra.procesapadel.domain.model.Player
 import java.nio.file.Files
 import java.nio.file.Path
@@ -21,6 +22,38 @@ class OutputWriter {
                 add("P$indice ${couple.player1} | ${couple.player2} | ${couple.level.uppercase()} | Horario: ${couple.time}")
 
                 add("")
+                indice + 1
+            }
+        }
+
+        Files.write(file, lines)
+    }
+
+    fun writeMatch(
+        matches: List<Match>,
+        outputDir: Path
+    ) {
+
+        val file = outputDir.resolve("torneo-partidos.txt")
+
+        val lines = buildList {
+
+            var indice = 1
+
+            for (match in matches) {
+
+                add("[Partido-$indice]")
+
+                add("Nivel: ${match.level.uppercase()}")
+
+                add("Franja: ${match.time}")
+
+                add("P${indice * 2 - 1}: ${match.couple1.player1} / ${match.couple1.player2}")
+
+                add("P${indice * 2}: ${match.couple2.player1} / ${match.couple2.player2}")
+
+                add("")
+
                 indice + 1
             }
         }
